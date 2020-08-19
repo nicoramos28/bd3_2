2922e9082fd55e54d6f24136b073642da547dfc4
Nico Ramos <nahuelramos.marset@gmail.com>
2020-08-18 21:03:40 -0300

    Ejercicio 2 completo

--- p2/src/com/practico2/persistence/PruebaAccesoBD.java
+++ p2/src/com/practico2/persistence/PruebaAccesoBD.java
@@ -22,43 +22,76 @@ public class PruebaAccesoBD {
         String db = properties.getProperty("db-name");
         String driver = "com.mysql.jdbc.Driver";
 
-        System.out.println("PROPERTIES ARE " + username + " " + host + " " + pass + " " + db + " ");
-        try {
-            Class.forName(driver);
+        /*** Ejercicio 1 ***/
+//        try {
+//            Class.forName(driver);
+//
+//            /* 2. una vez cargado el driver, me conecto con la base de datos */
+//            String url = "jdbc:mysql://"+ host + "/" + db;
+//            Connection con = DriverManager.getConnection(url, username, pass);
+//
+//            /* 3. creo un PreparedStatement para insertar una persona en base de datos */
+//            String insert = "INSERT into Personas values (?)";
+//            PreparedStatement pstmt = con.prepareStatement(insert);
+//            pstmt.setString(1, "Juan");
+//
+//            /* 4. ejecuto la sentencia de insercion y cierro el PreparedStatement */
+//            int cant = pstmt.executeUpdate();
+//            pstmt.close();
+//            System.out.println("Resultado del Insert :" + insert + "\n");
+//            System.out.println("Cantidad de filas afectadas "+cant+"\n");
+//
+//            /* 5. creo un Statement para listar todas las personas de la base de datos */
+//            Statement stmt = con.createStatement();
+//            String query = "Select * from Personas";
+//
+//            /* 6. ejecuto la consulta, listo las personas y cierro el ResultSet y el Statement */
+//            ResultSet result = stmt.executeQuery(query);
+//            System.out.println("Resultado de query : " + query);
+//            while (result.next()){
+//                System.out.println("\nPersona nombre :" + result.getString("nombre").trim());
+//            }
+//            result.close();
+//            stmt.close();
+//
+//            /* 7. por ultimo, cierro la conexion con la base de datos */
+//            con.close();
+//
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }
 
-            /* 2. una vez cargado el driver, me conecto con la base de datos */
-            String url = "jdbc:mysql://"+ host + "/" + db;
-            Connection con = DriverManager.getConnection(url, username, pass);
+        /*** Ejercicio 2 ***/
+//
+//        try {
+//            Class.forName(driver);
+//            String url = "jdbc:mysql://"+ host + "/" + db;
+//            Connection connection = DriverManager.getConnection(url, username, pass);
+//
+//            String sql1 = "Create table Personas (cedula int, nombre varchar(45), apellido varchar(45), primary key (cedula))";
+//            String sql2 = "Create table Maestras (cedula int, grupo varchar(45), primary key (cedula), FOREIGN KEY (cedula) REFERENCES Personas(cedula))";
+//            String sql3 = "Create table Alumnos (cedula int, cedulaMaestra varchar(45), primary key (cedula), FOREIGN KEY (cedula) REFERENCES Personas(cedula))";
+//
+//            Statement stmt1 = connection.createStatement();
+//            Statement stmt2 = connection.createStatement();
+//            Statement stmt3 = connection.createStatement();
+//
+//            int result1 = stmt1.executeUpdate(sql1);
+//            int result2 = stmt2.executeUpdate(sql2);
+//            int result3 = stmt3.executeUpdate(sql3);
+//
+//            System.out.println("RESULT QUERY 1 : " + result1);
+//            System.out.println("RESULT QUERY 1 : " + result2);
+//            System.out.println("RESULT QUERY 1 : " + result3);
+//
+//            stmt1.close();
+//            stmt2.close();
+//            stmt3.close();
+//            connection.close();
+//
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }
 
-            /* 3. creo un PreparedStatement para insertar una persona en base de datos */
-            String insert = "INSERT into Personas values (?)";
-            PreparedStatement pstmt = con.prepareStatement(insert);
-            pstmt.setString(1, "Juan");
-
-            /* 4. ejecuto la sentencia de insercion y cierro el PreparedStatement */
-            int cant = pstmt.executeUpdate();
-            pstmt.close();
-            System.out.println("Resultado del Insert :" + insert + "\n");
-            System.out.println("Cantidad de filas afectadas "+cant+"\n");
-
-            /* 5. creo un Statement para listar todas las personas de la base de datos */
-            Statement stmt = con.createStatement();
-            String query = "Select * from Personas";
-
-            /* 6. ejecuto la consulta, listo las personas y cierro el ResultSet y el Statement */
-            ResultSet result = stmt.executeQuery(query);
-            System.out.println("Resultado de query : " + query);
-            while (result.next()){
-                System.out.println("\nPersona nombre :" + result.getString("nombre").trim());
-            }
-            result.close();
-            stmt.close();
-
-            /* 7. por ultimo, cierro la conexion con la base de datos */
-            con.close();
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
